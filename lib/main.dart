d1b6d2f38ec420b87a32ee0dc83b0e7ce647d0a1
王喻 <wangyu@317hu.com>
2019-07-17 19:15:01 +0800

    第一版提交

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,43 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title:'wangyu',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('welcome wangyu'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          child: Text('hello world'),
         ),
       ),
     );
   }
+  
 }
+
+//import 'package:flutter/material.dart';
+//
+//void main() => runApp(MyApp());
+//
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Welcome to Flutter',
+//      home: Scaffold(
+//        appBar: AppBar(
+//          title: Text('Welcome to Flutter'),
+//        ),
+//        body: Center(
+//          child: Text('Hello World'),
+//        ),
+//      ),
+//    );
+//  }
+//}
 
