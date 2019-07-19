f95230d17cbd04978af6c40006b102b595f4809a
王喻 <wangyu@317hu.com>
2019-07-19 11:23:40 +0800

    修改内容：Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,36 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'dart:io';
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
 
-  //获取数据 此方法需要异步执行async/await
-  void getHttpClientData() async {
-    try {
-      //实例化一个HttpClient对象
-      HttpClient httpClient = HttpClient();
+void main() {
+  runApp(
+      new MaterialApp(
+        title: 'Container demo',
+        home: new ContainerDemo(),
+      )
+  );
+}
 
-      //发起请求 (IP + PORT + 请求接口)
-      HttpClientRequest request = await httpClient.getUrl(
-          Uri.parse("https://t.weater.sojson.com/api/weather/city/101030100"));
-
-      //等待服务器返回数据
-      HttpClientResponse response = await request.close();
-
-      //使用utf8.decoder从response里解析数据
-      var result = await response.transform(utf8.decoder).join();
-      //输出响应头
-      print(result);
-
-      //httpClient关闭
-      httpClient.close();
-
-    } catch (e) {
-      print("请求失败：$e");
-    } finally {
-
-    }
-  }
-
+class ContainerDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'HttpClient请求',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('HttpClient请求'),
+    // TODO: implement build
+    return new Center(
+
+      child: new Container(
+        width: 200.0,
+        height: 200.0,
+        decoration: new BoxDecoration(
+            color: Colors.white,
+            border: new Border.all(
+              color: Colors.green,
+              width: 8.0,
+            ),
+            borderRadius: const BorderRadius.all(const  Radius.circular(48.0))
         ),
-        body: Center(
-          child: RaisedButton(
-            child: Text("发起HttpClient请求"),
-            onPressed: getHttpClientData,
-          ),
+        child: new Text(
+          'Flutter',
+          textAlign: TextAlign.center,
         ),
       ),
     );
