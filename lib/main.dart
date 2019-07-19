2dcd0e368101d3cdf2fc471f3f129e7861e041d5
王喻 <wangyu@317hu.com>
2019-07-19 09:58:47 +0800

    修改内容：引入第三方包

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,27 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final appName = '自定义主题';
-
     return MaterialApp(
-      title: appName,
-      //主题配置
-      theme: ThemeData(
-        //应用程序整体主题的亮度
-        brightness: Brightness.light,
-        //App主要部分的背景色
-        primaryColor: Colors.lightGreen[600],
-        //前景色（文本、按钮等）
-        accentColor: Colors.orange[600],
-      ),
-      home: MyHomePage(
-        title: appName,
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  MyHomePage({Key key, @required this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Container(
-          //获取主题的accentColor
-          color: Theme.of(context).accentColor,
-          child: Text(
-            '带有背景颜色的文本组件',
-            //获取主题的文本样式
-            style: Theme.of(context).textTheme.title,
-          ),
+      title: '使用第三方包示例',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('使用第三方包示例'),
         ),
-      ),
-      floatingActionButton: Theme(
-        //使用copyWith的方式获取accentColor
-        data: Theme.of(context).copyWith(accentColor: Colors.grey),
-        child: FloatingActionButton(
-          onPressed: null,
-          child: Icon(Icons.computer),
+        body: Center(
+          child: RaisedButton(
+            onPressed: () {
+              //指定url并发起请求
+              const url = 'https://www.baidu.com';
+              //调用url_launcher包里的launch方法
+              launch(url);
+            },
+            child: Text('打开百度'),
+          ),
         ),
       ),
     );
