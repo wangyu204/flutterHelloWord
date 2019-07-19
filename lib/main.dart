6436bdb8c88d6086034c564269854f18bb2c662d
王喻 <wangyu@317hu.com>
2019-07-19 19:26:16 +0800

    修改内容：Align对齐布局

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,68 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  new MaterialApp(
-    title: 'Padding填充布局示例',
-    home: new LayoutDemo(),
-  ),
-);
-
 class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Padding填充布局示例'),
+        title: new Text('Align对齐布局示例'),
       ),
-      body: new Center(
-
-        child: new Container(
-          width: 300.0,
-          height: 300.0,
-          padding: EdgeInsets.all(60.0),//容器填充上下左右设置为60.0
-          decoration: new BoxDecoration(
-            color: Colors.white,
-            border: new Border.all(
-              color: Colors.green,
-              width: 8.0,
-            ),
-          ),
-          child: new Container(
-            width: 200.0,
-            height: 200.0,
-            decoration: new BoxDecoration(
-              color: Colors.white,
-              border: new Border.all(
-                color: Colors.blue,
-                width: 8.0,
-              ),
-            ),
-            child: new FlutterLogo(),
+      body: new Stack(children: <Widget>[
+        //左上角
+        new Align(
+          alignment: new FractionalOffset(0.0, 0.0),
+          child: new Image.asset(
+            'images/1.jpeg',
+            width: 128.0,
+            height: 128.0,
           ),
         ),
-      ),
+        //右上角
+        new Align(
+          alignment: FractionalOffset(1.0, 0.0),
+          child: new Image.asset(
+            'images/1.jpeg',
+            width: 128.0,
+            height: 128.0,
+          ),
+        ),
+        //水平垂直方向居中
+        new Align(
+          alignment: FractionalOffset.center,
+          child: new Image.asset(
+            'images/3.jpeg',
+            width: 128.0,
+            height: 128.0,
+          ),
+        ),
+        //左下角
+        new Align(
+          alignment: FractionalOffset.bottomLeft,
+          child: new Image.asset(
+            'images/2.jpeg',
+            width: 128.0,
+            height: 128.0,
+          ),
+        ),
+        //右下角
+        new Align(
+          alignment: FractionalOffset.bottomRight,
+          child: new Image.asset(
+            'images/2.jpeg',
+            width: 128.0,
+            height: 128.0,
+          ),
+        ),
+      ]),
     );
   }
 }
 
-
-
+void main() {
+  runApp(
+    new MaterialApp(
+      title: 'Align对齐布局示例',
+      home: new LayoutDemo(),
+    ),
+  );
+}
