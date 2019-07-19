9911cd88e4faa51eafb97649de68b7f84c739089
王喻 <wangyu@317hu.com>
2019-07-19 13:53:45 +0800

    修改内容：Scaffold脚手架组件

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MyApp());
-}
+void main() => runApp(
+  new MaterialApp(
+    title: 'Scaffold脚手架组件示例',
+    home: new LayoutDemo(),
+  ),
+);
 
-class MyApp extends StatelessWidget {
-  //这是整个应用的主组件
+class LayoutDemo extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new MyHomePage(),
-      title: 'MaterialApp示例',
-      routes: {
-        '/first': (BuildContext context) => FirstPage(), //添加路由
-        '/second': (BuildContext context) => SecondPage(),
-      },
-      initialRoute: '/first',//初始路由页面为first页面
-    );
-  }
-}
 
-//这是一个可改变的Widget
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: Text('MaterialApp示例'),
+      //头部元素 比如：左侧返回按钮 中间标题 右侧菜单
+      appBar: AppBar(
+        title: Text('Scaffold脚手架组件示例'),
       ),
+      //视图内容部分
       body: Center(
-        child: Text(
-          '主页',
-          style: TextStyle(fontSize: 28.0),
-        ),
+        child: Text('Scaffold'),
       ),
-    );
-  }
-}
-
-//第一个路由页面
-class FirstPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: Text('这是第一页'),
+      //底部导航栏
+      bottomNavigationBar: BottomAppBar(
+        child: Container(height: 50.0,),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //路由跳转到第二个页面
-            Navigator.pushNamed(context, '/second');
-          },
-          child: Text(
-            '这是第一页',
-            style: TextStyle(fontSize: 28.0),
-          ),
-        ),
+      //添加FAB按钮
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: '增加',
+        child: Icon(Icons.add),
       ),
-    );
-  }
-}
-
-//第二个路由页面
-class SecondPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: Text('这是第二页'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //路由跳转到第一个页面
-            Navigator.pushNamed(context, '/first');
-          },
-          child: Text(
-            '这是第二页',
-            style: TextStyle(fontSize: 28.0),
-          ),
-        ),
-      ),
+      //FAB按钮居中展示
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
