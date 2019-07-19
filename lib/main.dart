aca99f2e173b865b046981d19f41b0d6847fecd1
王喻 <wangyu@317hu.com>
2019-07-19 14:51:38 +0800

    修改内容：Drawer(抽屉组件）

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,64 @@
 import 'package:flutter/material.dart';
 
-class TabBarSample extends StatelessWidget {
+void main() => runApp(
+  new MaterialApp(
+    title: 'Drawer抽屉组件示例',
+    home: new LayoutDemo(),
+  ),
+);
+
+class LayoutDemo extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      //添加DefaultTabController关联TabBar及TabBarView
-      home: new DefaultTabController(
-        length: items.length,//传入选项卡数量
-        child: new Scaffold(
-          appBar: new AppBar(
-            title: const Text('TabBar选项卡示例'),
-            bottom: new TabBar(
-              isScrollable: true,//设置为可以滚动
-              tabs: items.map((ItemView item) {//迭代添加选项卡子项
-                return new Tab(
-                  text: item.title,
-                  icon: new Icon(item.icon),
-                );
-              }).toList(),
-            ),
-          ),
-          //添加选项卡视图
-          body: new TabBarView(
-            children: items.map((ItemView item) {//迭代显示选项卡视图
-              return new Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: new SelectedView(item: item),
-              );
-            }).toList(),
-          ),
-        ),
+
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('Drawer抽屉组件示例'),
       ),
-    );
-  }
-}
-
-//视图项数据
-class ItemView {
-  const ItemView({ this.title, this.icon });//构造方法
-  final String title;//标题
-  final IconData icon;//图标
-}
-
-//选项卡的类目
-const List<ItemView> items = const <ItemView>[
-  const ItemView(title: '自驾', icon: Icons.directions_car),
-  const ItemView(title: '自行车', icon: Icons.directions_bike),
-  const ItemView(title: '轮船', icon: Icons.directions_boat),
-  const ItemView(title: '公交车', icon: Icons.directions_bus),
-  const ItemView(title: '火车', icon: Icons.directions_railway),
-  const ItemView(title: '步行', icon: Icons.directions_walk),
-];
-
-//被选中的视图
-class SelectedView extends StatelessWidget {
-  const SelectedView({ Key key, this.item }) : super(key: key);
-
-  //视图数据
-  final ItemView item;
-
-  @override
-  Widget build(BuildContext context) {
-    final TextStyle textStyle = Theme.of(context).textTheme.display1;
-    return new Card(
-      color: Colors.white,
-      child: new Center(
-        child: new Column(
-          mainAxisSize: MainAxisSize.min,//垂直方向最小化处理
-          crossAxisAlignment: CrossAxisAlignment.center,//水平方向居中对齐
+      drawer: Drawer(
+        child: ListView(
           children: <Widget>[
-            new Icon(item.icon, size: 128.0, color: textStyle.color),
-            new Text(item.title, style: textStyle),
+            //设置用户信息 头像、用户名、Email等
+            UserAccountsDrawerHeader(
+              accountName: new Text(
+                "玄微子",
+              ),
+              accountEmail: new Text(
+                "xuanweizi@163.com",
+              ),
+              //设置当前用户头像
+              currentAccountPicture: new CircleAvatar(
+                backgroundImage: new AssetImage("images/1.jpeg"),
+              ),
+              onDetailsPressed: () {},
+              //属性本来是用来设置当前用户的其他账号的头像 这里用来当QQ二维码图片展示
+              otherAccountsPictures: <Widget>[
+                new Container(
+                  child: Image.asset('images/code.jpeg'),
+                ),
+              ],
+            ),
+            ListTile(
+              leading: new CircleAvatar(child: Icon(Icons.color_lens)),//导航栏菜单
+              title: Text('个性装扮'),
+            ),
+            ListTile(
+              leading: new CircleAvatar(child: Icon(Icons.photo)),
+              title: Text('我的相册'),
+            ),
+            ListTile(
+              leading: new CircleAvatar(child: Icon(Icons.wifi)),
+              title: Text('免流量特权'),
+            ),
           ],
         ),
       ),
     );
+
   }
 }
 
-void main() {
-  runApp(new TabBarSample());
-}
+
 
 
