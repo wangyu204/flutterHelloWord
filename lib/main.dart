d9a63eeb2249374e9a38b3f219791f6232878217
王喻 <wangyu@317hu.com>
2019-07-19 10:44:00 +0800

    修改内容：httpClient

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  //获取数据 此方法需要异步执行async/await
+  void getHttpClientData() async {
+    try {
+      //实例化一个HttpClient对象
+      HttpClient httpClient = HttpClient();
+
+      //发起请求 (IP + PORT + 请求接口)
+      HttpClientRequest request = await httpClient.getUrl(
+          Uri.parse("https://t.weater.sojson.com/api/weather/city/101030100"));
+
+      //等待服务器返回数据
+      HttpClientResponse response = await request.close();
+
+      //使用utf8.decoder从response里解析数据
+      var result = await response.transform(utf8.decoder).join();
+      //输出响应头
+      print(result);
+
+      //httpClient关闭
+      httpClient.close();
+
+    } catch (e) {
+      print("请求失败：$e");
+    } finally {
+
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'http请求示例',
+      title: 'HttpClient请求',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('http请求示例'),
+          title: Text('HttpClient请求'),
         ),
         body: Center(
           child: RaisedButton(
-            onPressed: () {
-              //请求后台url路径(IP + PORT + 请求接口)
-              var url = 'http://httpbin.org/';
-              //向后台发起get请求 response为返回对象
-              http.get(url).then((response) {
-                print("状态： ${response.statusCode}");
-                print("正文： ${response.body}");
-              });
-            },
-            child: Text('发起http请求'),
+            child: Text("发起HttpClient请求"),
+            onPressed: getHttpClientData,
           ),
         ),
       ),
