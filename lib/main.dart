30dc777ee81db8332d11db23e1836961707425c8
王喻 <wangyu@317hu.com>
2019-07-19 13:59:25 +0800

    修改内容：AppBar顶部导航栏

--- lib/main.dart
+++ lib/main.dart
@@ -2,37 +2,35 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
   new MaterialApp(
-    title: 'Scaffold脚手架组件示例',
+    title: 'AppBar应用按钮示例',
     home: new LayoutDemo(),
   ),
 );
 
 class LayoutDemo extends StatelessWidget {
 
+
   @override
   Widget build(BuildContext context) {
 
     return new Scaffold(
-      //头部元素 比如：左侧返回按钮 中间标题 右侧菜单
       appBar: AppBar(
-        title: Text('Scaffold脚手架组件示例'),
+        title: Text('AppBar应用按钮示例'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: '搜索',
+            onPressed: (){},
+          ),
+          IconButton(
+            icon: Icon(Icons.add),
+            tooltip: '添加',
+            onPressed: (){},
+          ),
+        ],
       ),
-      //视图内容部分
-      body: Center(
-        child: Text('Scaffold'),
-      ),
-      //底部导航栏
-      bottomNavigationBar: BottomAppBar(
-        child: Container(height: 50.0,),
-      ),
-      //添加FAB按钮
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: '增加',
-        child: Icon(Icons.add),
-      ),
-      //FAB按钮居中展示
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
     );
+
   }
 }
