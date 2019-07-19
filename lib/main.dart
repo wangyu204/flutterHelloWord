efe172a9505d7d0387119edff2f945591cdc2dd8
王喻 <wangyu@317hu.com>
2019-07-19 15:04:29 +0800

    修改内容：SimpleDialog组件

--- lib/main.dart
+++ lib/main.dart
@@ -2,45 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-//会控菜单项
-enum ConferenceItem { AddMember, LockConference, ModifyLayout, TurnoffAll }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title: 'PopupMenuButton组件示例',
+      title: 'SimpleDialog组件示例',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('PopupMenuButton组件示例'),
+          title: Text('SimpleDialog组件示例'),
         ),
         body: Center(
-          child: FlatButton(
-            onPressed: () {},
-            child: PopupMenuButton<ConferenceItem>(
-              onSelected: (ConferenceItem result) {},
-              itemBuilder: (BuildContext context) => //菜单项构造器
-                  <PopupMenuEntry<ConferenceItem>>[
-                const PopupMenuItem<ConferenceItem>(
-                  //菜单项
-                  value: ConferenceItem.AddMember,
-                  child: Text('添加成员'),
-                ),
-                const PopupMenuItem<ConferenceItem>(
-                  value: ConferenceItem.LockConference,
-                  child: Text('锁定会议'),
-                ),
-                const PopupMenuItem<ConferenceItem>(
-                  value: ConferenceItem.ModifyLayout,
-                  child: Text('修改布局'),
-                ),
-                const PopupMenuItem<ConferenceItem>(
-                  value: ConferenceItem.TurnoffAll,
-                  child: Text('挂断所有'),
-                ),
-              ],
-            ),
+          child: SimpleDialog(
+            title: const Text('对话框标题'),
+            children: <Widget>[
+              SimpleDialogOption(
+                onPressed: () {  },
+                child: const Text('第一行信息'),
+              ),
+              SimpleDialogOption(
+                onPressed: () {  },
+                child: const Text('第二行信息'),
+              ),
+            ],
           ),
+
         ),
       ),
     );
