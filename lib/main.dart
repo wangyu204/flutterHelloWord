31dc0ee6929ac9ac72da1fbe26c4893f632c02b2
王喻 <wangyu@317hu.com>
2019-07-19 15:31:04 +0800

    修改内容：CupertinoButton组件

--- lib/main.dart
+++ lib/main.dart
@@ -7,37 +7,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CupertinoAlertDialog组件示例',
+      title: 'CupertinoButton组件示例',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('CupertinoAlertDialog组件示例'),
+          title: Text('CupertinoButton组件示例'),
         ),
         body: Center(
-          child: CupertinoAlertDialog(
-            title: Text('提示'), //对话框标题
-            content: SingleChildScrollView(
-              //对话框内容部分
-              child: ListBody(
-                children: <Widget>[
-                  Text('是否要删除?'),
-                  Text('一旦删除数据不可恢复！'),
-                ],
-              ),
+          child: CupertinoButton(
+            child: Text(//按钮label
+              'CupertinoButton',
             ),
-            actions: <Widget>[
-              CupertinoDialogAction(
-                child: Text('确定'),
-                onPressed: () {},
-              ),
-              CupertinoDialogAction(
-                child: Text('取消'),
-                onPressed: () {},
-              ),
-            ],
+            color: Colors.blue,//按钮颜色
+            onPressed: (){},//按下事件回调
           ),
         ),
       ),
     );
   }
 }
-
