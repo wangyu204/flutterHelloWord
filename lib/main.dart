e5d428b6d7c34a46abb513f1765a5371251d36cb
王喻 <wangyu@317hu.com>
2019-07-19 19:20:02 +0800

    修改内容：save

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
   new MaterialApp(
-    title: 'Center居中布局示例',
+    title: 'Padding填充布局示例',
     home: new LayoutDemo(),
   ),
 );
@@ -12,16 +12,38 @@ class LayoutDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Center居中布局示例'),
+        title: new Text('Padding填充布局示例'),
       ),
       body: new Center(
-        child: new Text(
-          'Hello Flutter',
-          style: TextStyle(
-            fontSize: 36.0,
+
+        child: new Container(
+          width: 300.0,
+          height: 300.0,
+          padding: EdgeInsets.all(60.0),//容器填充上下左右设置为60.0
+          decoration: new BoxDecoration(
+            color: Colors.white,
+            border: new Border.all(
+              color: Colors.green,
+              width: 8.0,
+            ),
+          ),
+          child: new Container(
+            width: 200.0,
+            height: 200.0,
+            decoration: new BoxDecoration(
+              color: Colors.white,
+              border: new Border.all(
+                color: Colors.blue,
+                width: 8.0,
+              ),
+            ),
+            child: new FlutterLogo(),
           ),
         ),
       ),
     );
   }
 }
+
+
+
