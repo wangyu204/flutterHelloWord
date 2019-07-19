93e03e02339725f1bc881e21b60f0b2a9926e126
王喻 <wangyu@317hu.com>
2019-07-19 15:28:11 +0800

    修改内容：CupertinoActivityIndicator组件

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    title: 'Card布局示例',
-    home: new MyApp(),
-  ));
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    var card = new SizedBox(
-      height: 250.0,
-      child: new Card(
-        child: new Column(
-          children: <Widget>[
-            new ListTile(
-              title: new Text(
-                '深圳市南山区深南大道35号',style: new TextStyle(fontWeight: FontWeight.w300),
-              ),
-              subtitle: new Text('创想科技有限公司'),
-              leading: new Icon(
-                Icons.home,
-                color: Colors.lightBlue,
-              ),
-            ),
-            new Divider(),
-            new ListTile(
-              title: new Text(
-                '深圳市罗湖区沿海大道32号',style: new TextStyle(fontWeight: FontWeight.w300),
-              ),
-              subtitle: new Text('一木培训机构'),
-              leading: new Icon(
-                Icons.school,
-                color: Colors.lightBlue,
-              ),
-            ),
-            new Divider(),
-
-          ],
+    return MaterialApp(
+      title: 'CupertinoActivityIndicator示例',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('CupertinoActivityIndicator示例'),
         ),
-      ),
-    );
-
-
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Card布局示例'),
-      ),
-      body: new Center(
-        child: card,
+        body: Center(
+          child: CupertinoActivityIndicator(
+            radius: 60.0,//值越大加载的图形越大
+          ),
+        ),
       ),
     );
   }
