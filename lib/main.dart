bef8288c4d576896f012f4a1a6371665118834fa
王喻 <wangyu@317hu.com>
2019-07-19 19:29:22 +0800

    修改内容：Row水平布局

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,38 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(
+  new MaterialApp(
+    title: '水平布局示例',
+    home: new LayoutDemo(),
+  ),
+);
+
 class LayoutDemo extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Align对齐布局示例'),
+        title: new Text('水平布局示例'),
       ),
-      body: new Stack(children: <Widget>[
-        //左上角
-        new Align(
-          alignment: new FractionalOffset(0.0, 0.0),
-          child: new Image.asset(
-            'images/1.jpeg',
-            width: 128.0,
-            height: 128.0,
+      body: new Row(
+        children: <Widget>[
+          new Expanded(
+            child: new Text('左侧文本', textAlign: TextAlign.center),
           ),
-        ),
-        //右上角
-        new Align(
-          alignment: FractionalOffset(1.0, 0.0),
-          child: new Image.asset(
-            'images/1.jpeg',
-            width: 128.0,
-            height: 128.0,
+          new Expanded(
+            child: new Text('中间文本', textAlign: TextAlign.center),
           ),
-        ),
-        //水平垂直方向居中
-        new Align(
-          alignment: FractionalOffset.center,
-          child: new Image.asset(
-            'images/3.jpeg',
-            width: 128.0,
-            height: 128.0,
+          new Expanded(
+            child: new FittedBox(
+              fit: BoxFit.contain,
+              child: const FlutterLogo(),
+            ),
           ),
-        ),
-        //左下角
-        new Align(
-          alignment: FractionalOffset.bottomLeft,
-          child: new Image.asset(
-            'images/2.jpeg',
-            width: 128.0,
-            height: 128.0,
-          ),
-        ),
-        //右下角
-        new Align(
-          alignment: FractionalOffset.bottomRight,
-          child: new Image.asset(
-            'images/2.jpeg',
-            width: 128.0,
-            height: 128.0,
-          ),
-        ),
-      ]),
+        ],
+      ),
     );
-  }
-}
 
-void main() {
-  runApp(
-    new MaterialApp(
-      title: 'Align对齐布局示例',
-      home: new LayoutDemo(),
-    ),
-  );
+  }
 }
