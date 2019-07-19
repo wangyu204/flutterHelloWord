098420cfd7be6b8577b8609ebbc13e2139a792a9
王喻 <wangyu@317hu.com>
2019-07-19 15:29:45 +0800

    修改内容：CupertinoAlertDialog提示框组件

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CupertinoActivityIndicator示例',
+      title: 'CupertinoAlertDialog组件示例',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('CupertinoActivityIndicator示例'),
+          title: Text('CupertinoAlertDialog组件示例'),
         ),
         body: Center(
-          child: CupertinoActivityIndicator(
-            radius: 60.0,//值越大加载的图形越大
+          child: CupertinoAlertDialog(
+            title: Text('提示'), //对话框标题
+            content: SingleChildScrollView(
+              //对话框内容部分
+              child: ListBody(
+                children: <Widget>[
+                  Text('是否要删除?'),
+                  Text('一旦删除数据不可恢复！'),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              CupertinoDialogAction(
+                child: Text('确定'),
+                onPressed: () {},
+              ),
+              CupertinoDialogAction(
+                child: Text('取消'),
+                onPressed: () {},
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
+
