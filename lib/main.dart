1171b8d5d58410fe61ed3c29bd275429b0410e81
王喻 <wangyu@317hu.com>
2019-07-23 18:31:24 +0800

    修改内容：不灵活的row布局

--- lib/main.dart
+++ lib/main.dart
@@ -11,41 +11,23 @@ class MyApp extends StatelessWidget {
         appBar: new AppBar(
           title: new Text('title'),
         ),
-        body: GridView(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            mainAxisSpacing: 2.0,
-            crossAxisSpacing: 2.0,
-            childAspectRatio: 0.7,
-          ),
+        body: new Row(
           children: <Widget>[
-            new Image.network(
-                'http://img5.mtime.cn/mt/2018/10/22/104316.77318635_180X260X4.jpg',
-                fit: BoxFit.cover),
-            new Image.network(
-                'http://img5.mtime.cn/mt/2018/10/10/112514.30587089_180X260X4.jpg',
-                fit: BoxFit.cover),
-            new Image.network(
-                'http://img5.mtime.cn/mt/2018/11/13/093605.61422332_180X260X4.jpg',
-                fit: BoxFit.cover),
-            new Image.network(
-                'http://img5.mtime.cn/mt/2018/11/07/092515.55805319_180X260X4.jpg',
-                fit: BoxFit.cover),
-            new Image.network(
-                'http://img5.mtime.cn/mt/2018/11/21/090246.16772408_135X190X4.jpg',
-                fit: BoxFit.cover),
-            new Image.network(
-                'http://img5.mtime.cn/mt/2018/11/17/162028.94879602_135X190X4.jpg',
-                fit: BoxFit.cover),
-            new Image.network(
-                'http://img5.mtime.cn/mt/2018/11/19/165350.52237320_135X190X4.jpg',
-                fit: BoxFit.cover),
-            new Image.network(
-                'http://img5.mtime.cn/mt/2018/11/16/115256.24365160_180X260X4.jpg',
-                fit: BoxFit.cover),
-            new Image.network(
-                'http://img5.mtime.cn/mt/2018/11/20/141608.71613590_135X190X4.jpg',
-                fit: BoxFit.cover),
+            new RaisedButton(
+              onPressed: () {},
+              color: Colors.redAccent,
+              child: new Text('red'),
+            ),
+            new RaisedButton(
+              onPressed: () {},
+              color: Colors.yellowAccent,
+              child: new Text('yellow'),
+            ),
+            new RaisedButton(
+              onPressed: () {},
+              color: Colors.lightBlue,
+              child: new Text('lightBlue'),
+            ),
           ],
         ),
       ),
