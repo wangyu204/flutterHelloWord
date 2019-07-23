333fd13b3cdc9685954b7f030b24c4973e5ea572
王喻 <wangyu@317hu.com>
2019-07-23 19:05:00 +0800

    修改内容：stack 用Positoned绝对定位

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var stack = new Stack(
-      alignment: const FractionalOffset(0.5, 0.8),
       children: <Widget>[
         new CircleAvatar(
           backgroundImage: new NetworkImage(
               'http://blogimages.jspang.com/blogtouxiang1.jpg'),
           radius: 100,
         ),
-        new Container(
-          decoration: new BoxDecoration(
-            color: Colors.lightBlue,
-          ),
-          padding: EdgeInsets.all(5.0),
-          child: Text('wangyu'),
+        new Positioned(
+          child: new Text('wangyu.com'),
+          top: 10.0,
+          left: 60.0,
+        ),
+        new Positioned(
+          child: new Text('技术联盟'),
+          bottom: 10.0,
+          right: 10.0,
         ),
       ],
     );
