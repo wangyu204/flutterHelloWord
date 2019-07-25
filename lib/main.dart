1ef4477a42721c329cd2c533e60fe70783bf50a5
王喻 <wangyu@317hu.com>
2019-07-25 15:15:18 +0800

    修改内容：展开闭合效果ExpansionTile

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'wrap_demo.dart';
+import 'expansion_tile_demo.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,9 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Demo",
-      theme: ThemeData.light(),
-      home: WrapDemo(),
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: ExpansionTileDemo(),
     );
   }
 }
