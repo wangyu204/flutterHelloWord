e1bfb6194d00ffb3bffb73be080dcf88f2d0cbe8
王喻 <wangyu@317hu.com>
2019-07-25 12:26:42 +0800

    修改内容：保持页面状态

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'frosted_glass_demo.dart';
+import 'keep_alive_demo.dart';
 
 void main() => runApp(new MyApp());
 
@@ -14,8 +14,58 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        body: FrostedGlassDemo(),
+        body: KeepAliveDemo(),
       ),
+    );
+  }
+}
+
+class KeepAliveDemo extends StatefulWidget {
+  @override
+  _KeepAliveDemoState createState() => _KeepAliveDemoState();
+}
+
+class _KeepAliveDemoState extends State<KeepAliveDemo>
+    with SingleTickerProviderStateMixin {
+  TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('keep alive demo'),
+        bottom: TabBar(
+          controller: _controller,
+          tabs: [
+            Tab(
+              icon: Icon(Icons.directions_car),
+            ),
+            Tab(
+              icon: Icon(Icons.directions_transit),
+            ),
+            Tab(
+              icon: Icon(Icons.directions_bike),
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(controller: _controller, children: <Widget>[
+        MyHomePage(),
+        MyHomePage(),
+        MyHomePage(),
+      ]),
     );
   }
 }
