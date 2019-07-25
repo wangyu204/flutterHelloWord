eabef6fc7bf81040db806e92d9e00180492ded92
王喻 <wangyu@317hu.com>
2019-07-25 18:26:28 +0800

    修改内容：贝塞尔曲线扩展 波浪线

--- lib/custom_clipper.dart
+++ lib/custom_clipper.dart
@@ -10,7 +10,7 @@ class HomePage extends StatelessWidget {
       body: Column(
         children: <Widget>[
           ClipPath(
-            clipper: BottomClipper(),
+            clipper: BottomClipper2(),
             child: Container(
               color: Colors.deepPurpleAccent,
               height: 200.0,
@@ -22,19 +22,23 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class BottomClipper extends CustomClipper<Path> {
+class BottomClipper2 extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = Path();
     path.lineTo(0, 0);
-    path.lineTo(0, size.height - 30);
-
-    var firstControlPoint = Offset(size.width / 2, size.height);
-    var secondControlPoint = Offset(size.width, size.height - 30);
+    path.lineTo(0, size.height - 40);
+    var firstControlPoint = Offset(size.width / 4, size.height);
+    var firstEndPoint = Offset(size.width / 2, size.height - 40);
     path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
-        secondControlPoint.dx, secondControlPoint.dy);
+        firstEndPoint.dx, firstEndPoint.dy);
 
-    path.lineTo(size.width, size.height - 30);
+    var secondControlPoint = Offset(size.width / 4 * 3, size.height - 90);
+    var secondEndPoint = Offset(size.width, size.height - 40);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondEndPoint.dx, secondEndPoint.dy);
+
+    path.lineTo(size.width, size.height - 40);
     path.lineTo(size.width, 0);
 
     return path;
