c8243efaaf00f2e3fa79c47e4a6f2455c9025a3c
王喻 <wangyu@317hu.com>
2019-07-25 14:18:33 +0800

    修改内容：搜索条

---
+++ lib/asset.dart
@@ -0,0 +1,3 @@
+const searchList = ['jiejie-dct', 'jiejie-xmy', 'gege-欧巴', 'gege-小鲜肉'];
+
+const recentSuggest = ["推荐1", "推荐2"];
--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,16 @@
 import 'package:flutter/material.dart';
 
-import 'keep_alive_demo.dart';
+import 'search_bar_demo.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '不规则',
-      //自定义主题样本
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        body: KeepAliveDemo(),
-      ),
-    );
-  }
-}
-
-class KeepAliveDemo extends StatefulWidget {
-  @override
-  _KeepAliveDemoState createState() => _KeepAliveDemoState();
-}
-
-class _KeepAliveDemoState extends State<KeepAliveDemo>
-    with SingleTickerProviderStateMixin {
-  TabController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = TabController(length: 3, vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('keep alive demo'),
-        bottom: TabBar(
-          controller: _controller,
-          tabs: [
-            Tab(
-              icon: Icon(Icons.directions_car),
-            ),
-            Tab(
-              icon: Icon(Icons.directions_transit),
-            ),
-            Tab(
-              icon: Icon(Icons.directions_bike),
-            ),
-          ],
-        ),
-      ),
-      body: TabBarView(controller: _controller, children: <Widget>[
-        MyHomePage(),
-        MyHomePage(),
-        MyHomePage(),
-      ]),
+      title: 'Flutter Demo',
+      theme: ThemeData.light(),
+      home: SearchBarDemo(),
     );
   }
 }
