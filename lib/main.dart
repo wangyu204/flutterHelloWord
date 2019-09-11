107b17d3922c3c6cbb8d4b74a8d3a02454cab0ba
王喻 <wangyu@317hu.com>
2019-09-11 17:45:29 +0800

    修改人：王喻  修改内容：页面跳转返回数据

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,78 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'flutter demo',
+      title: '页面跳转返回参数',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: DraggableDemo(),
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("页面跳转返回参数"),
+      ),
+      body: Center(
+        child: new RouteButton(),
+      ),
+    );
+  }
+}
+
+class RouteButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {
+        _navigateToSecondPage(context);
+      },
+      child: Text('跳转到第二个页面'),
+    );
+  }
+
+  _navigateToSecondPage(BuildContext context) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => new SecondPage()),
+    );
+    Scaffold.of(context).showSnackBar(new SnackBar(content: Text("$result")));
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("选择一条数据"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            buildPadding(context, 'hi google'),
+            buildPadding(context, 'hi baidu'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding buildPadding(BuildContext context, String name) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: RaisedButton(
+        onPressed: () {
+          Navigator.pop(context, name);
+        },
+        child: Text(name),
+      ),
     );
   }
 }
