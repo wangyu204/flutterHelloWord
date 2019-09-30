8dc6b93cc6b99988542dd9bdf371a9c382fe3cfd
王喻 <wangyu@317hu.com>
2019-09-30 14:36:11 +0800

    save

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,214 @@
 import 'package:flutter/material.dart';
 
-import 'draggable_demo.dart';
-import 'models/index.dart';
+import 'dart:math' as Math;
 
-//void main() => runApp(MyApp());
+import 'package:binding_helper/binding_helper.dart';
 
-void main() {
-  var u = User.fromJson({"name": "Jack", "age": 16, "+1": 20});
-  print(u.loved); // 20
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '页面跳转返回参数',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: FirstPage(),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class FirstPage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("页面跳转返回参数"),
-      ),
-      body: Center(
-        child: new RouteButton(),
-      ),
-    );
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+List<Color> colors = [
+  Colors.green,
+  Colors.yellowAccent,
+  Colors.black12,
+  Colors.greenAccent,
+  Colors.redAccent
+];
+
+typedef void TapRect(Rect rect);
+
+class RectGetter extends StatefulWidget {
+  final Widget child;
+  final TapRect tapRect;
+
+  RectGetter({this.child, this.tapRect});
+
+  @override
+  State<StatefulWidget> createState() {
+    return new _RectGetterState();
   }
 }
 
-class RouteButton extends StatelessWidget {
+class _RectGetterState extends State<RectGetter> {
+  Rect _rect;
+
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      onPressed: () {
-        _navigateToSecondPage(context);
+    return new GestureDetector(
+      onTap: () {
+        widget.tapRect(_rect);
       },
-      child: Text('跳转到第二个页面'),
+      behavior: HitTestBehavior.opaque,
+      child: new RectProvider(
+        child: widget.child,
+        onGetRect: (Rect rect) {
+          _rect = rect;
+        },
+      ),
     );
-  }
-
-  _navigateToSecondPage(BuildContext context) async {
-    final result = await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => new SecondPage()),
-    );
-    Scaffold.of(context).showSnackBar(new SnackBar(content: Text("$result")));
   }
 }
 
-class SecondPage extends StatelessWidget {
+class SecondPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new SecondPageState();
+  }
+}
+
+class Item extends StatefulWidget {
+  final int i;
+  final double height;
+  Item({this.i, this.height});
+
+  @override
+  State<StatefulWidget> createState() {
+    return new ItemState();
+  }
+}
+
+class ItemState extends State<Item> {
+  Rect _rect;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("选择一条数据"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            buildPadding(context, 'hi google'),
-            buildPadding(context, 'hi baidu'),
-          ],
+    return new Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        new Container(
+          height: _rect?.height ?? 20.0,
+          width: 20.0,
+          color: widget.i % 2 == 0 ? Colors.blueAccent : Colors.greenAccent,
         ),
-      ),
+        new RectProvider(
+            child: new SizedBox(
+              width: 100.0,
+              height: widget.height,
+              child: new Container(
+                color: widget.i % 2 == 0 ? Colors.redAccent : Colors.black12,
+              ),
+            ),
+            onGetRect: (Rect rect) {
+              if (_rect == null ||
+                  _rect.width != rect.width ||
+                  _rect.height != rect.height) {
+                setState(() {
+                  _rect = rect;
+                });
+              }
+            })
+      ],
     );
   }
+}
 
-  Padding buildPadding(BuildContext context, String name) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: RaisedButton(
-        onPressed: () {
-          Navigator.pop(context, name);
-        },
-        child: Text(name),
+/// we draw a line with height of the text
+class SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("ListViewTest"),
       ),
+      body: new ListView.builder(
+          itemCount: 100,
+          itemBuilder: (BuildContext c, int i) {
+            return new Item(
+              i: i,
+              height: new Math.Random().nextInt(200).toDouble(),
+            );
+          }),
+    );
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  GlobalKey globalKey = new GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> list = [];
+    for (int i = 0; i < 100; ++i) {
+      list.add(
+        new Container(
+            color: colors[i % colors.length],
+            child: new Padding(
+              padding:
+                  new EdgeInsets.all(new Math.Random().nextInt(10).toDouble()),
+              child: new RectGetter(
+                tapRect: (Rect rect) {
+                  ScaffoldState state = globalKey.currentState;
+
+                  state.showSnackBar(new SnackBar(
+                      content: new SizedBox(
+                    height: 30.0,
+                    child: new Text(
+                      "The rect is $rect",
+                      style: new TextStyle(color: Colors.white),
+                    ),
+                  )));
+                },
+                child: new SizedBox(
+                  height: 30.0 + new Math.Random().nextInt(70).toDouble(),
+                  width: 30.0 + new Math.Random().nextInt(70).toDouble(),
+                ),
+              ),
+            )),
+      );
+    }
+
+    return new Scaffold(
+      key: globalKey,
+      appBar: new AppBar(
+        title: new Text(widget.title),
+        actions: <Widget>[
+          new InkWell(
+            child: new Padding(
+              padding: new EdgeInsets.all(10.0),
+              child: new Center(
+                child: new Text("ListView"),
+              ),
+            ),
+            onTap: () {
+              Navigator.push(context,
+                  new MaterialPageRoute(builder: (BuildContext context) {
+                return new SecondPage();
+              }));
+            },
+          )
+        ],
+      ),
+      body: new Stack(
+        children: <Widget>[
+          new SingleChildScrollView(
+            child: new Wrap(
+              children: list,
+            ),
+          ),
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
